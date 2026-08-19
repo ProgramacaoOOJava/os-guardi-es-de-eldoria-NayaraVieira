29b09c9f81f3744d69442cdf978f8e3ec7ad10cb
NayaraVieira <90634395+NayaraVieira@users.noreply.github.com>
2026-08-19 21:25:42 +0000

    atividade do nivel novato

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi1 = new Personagem("Aragorn", "Guerreiro", 10, 100, 50.0);
+        Personagem heroi2 = new Personagem("Gandalf", "Mago", 15, 80, 70.0);
+        Personagem heroi3 = new Personagem("Arthemis", "Arqueira", 5, 80, 12.5);
+        Personagem heroi4 = new Personagem("Lyrenna", "Maga", 8, 60, 30.0);
+        Personagem heroi5 = new Personagem("Thorin", "Guerreiro", 12, 90, 55.0);
+        Personagem heroi6 = new Personagem("Legolas", "Arqueiro", 7, 75, 25.0);
 
-        .
-        .
-        .
+        System.out.println("Status do Personagem 1:");
+        heroi1.exibirStatus();
 
+        System.out.println("\nStatus do Personagem 2:");
+        heroi2.exibirStatus();
+
+        System.out.println("\nStatus do Personagem 3:");
+        heroi3.exibirStatus();
+
+        System.out.println("\nStatus do Personagem 4:");
+        heroi4.exibirStatus();
+
+        System.out.println("\nStatus do Personagem 5:");
+        heroi5.exibirStatus();
+
+        System.out.println("\nStatus do Personagem 6:");
+        heroi6.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,24 +1,23 @@
 public class Personagem {
     String nome;
-    String classe;
+    String classe; // Classe do personagem (ex: Guerreiro, Mago, Arqueiro)
     int nivel;
     int pontosDeVida;
     double poderBase;
 
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
-
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     }
 
     public void exibirStatus() {
-        System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
-
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
     }
 }
-
